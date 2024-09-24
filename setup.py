cbba1a18842721a2f54a042db9759ef8586e0df4
Jose Angel <jaalvarez2818@chagodtour.travel>
2024-09-24 22:58:40 +0200

    Authenticate Availability

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jaalvarez2818_hotetec_sdk',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     install_requires=[
         'requests',
