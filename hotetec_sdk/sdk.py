9b0dd00d361a81c5935dff38951095f87c47f23d
Jose Angel <jaalvarez2818@chagodtour.travel>
2025-02-05 16:36:10 +0100

    Fix Hotetec

--- hotetec_sdk/sdk.py
+++ hotetec_sdk/sdk.py
@@ -238,11 +238,9 @@ class HotetecSDK:
                     reserve_data = [reserve_data]
 
                 payment_info = response.get('infrsr', {}).get('infrpg', {})
-                if payment_info:
-                    if type(payment_info) is list:
-                        payment_info = next(
-                            filter(lambda x: x.get('inffpg', {}).get('infopg', {}).get('tippag') == 'C', payment_info),
-                            None)
+
+                if type(payment_info) is dict:
+                    payment_info = [payment_info]
 
                 return {'response': {
                     'start_date': response.get('resser', {}).get('fecini'),
@@ -257,8 +255,8 @@ class HotetecSDK:
                     },
                     'payment': {
                         'currency': response.get('coddiv'),
-                        'total_amount': payment_info.get('inffpg', {}).get('imptot'),
-                        'limit_date': payment_info.get('inffpg', {}).get('fecpag'),
+                        'total_amount': sum([float(pay.get('inffpg', {}).get('imptot')) for pay in payment_info]),
+                        'limit_date': min([pay.get('inffpg', {}).get('fecpag') for pay in payment_info]),
                     },
                     'rooms': [
                         {
